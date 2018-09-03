091b110991ff581236eb96f1707268c22b669dee
root <root@localhost.localdomain>
2018-09-03 20:24:15 -0300

    WIP conexão com o pg e mongo

--- handler.js
+++ handler.js
@@ -3,8 +3,10 @@
 let atlas_connection_uri
 let cachedDb = null
 const MongoClient = require('mongodb').MongoClient
-const pg = require('pg')
+// const pg = require('pg')
+// let client = null
 const exchangeData = (event, context, callback) => {
+  console.log(process.env.MONGODB_URI);
   let uri = process.env.MONGODB_URI
   if (atlas_connection_uri != null) {
     processEvent(event, context, callback)
@@ -14,14 +16,17 @@ const exchangeData = (event, context, callback) => {
     processEvent(event, context, callback)
   }
 }
+const connectToPostgres = (uri)=>{
+  // if(client === null) return client = new pg.Psentool(process.env.PG_URI)
+  // return client
+}
 
 const connectToDatabase = (uri) => {
-
   if (cachedDb && cachedDb.serverConfig.isConnected()) {
     return Promise.resolve(cachedDb)
   }
   const dbName = process.env.MONGODB_DB
-  return MongoClient.connect(uri)
+  return MongoClient.connect(process.env.DB)
     .then(client => { cachedDb = client.db(dbName); return cachedDb })
 }
 
@@ -36,14 +41,26 @@ const processEvent = (event, context, callback) => {
     .then((notificacao) => {
       let { notifications } = notificacao
       notifications.map(notification => {
+        let arrayRetorno = []
         let objeto = {}
         objeto['sent'] = false
-        if (notification.send) {
-          objeto['sent'] = true
+        if(notification.sent){
+          objeto['enviado'] = true
+          objeto['dataEnvio'] = notification.sentAt
+        }
+
+        if (notification.erro) {
+          objeto['enviado'] = false
+          objeto['erro']= notification.erro
         }
         arrayRetorno.push(objeto)
-        callback(null,arrayRetorno)
+        return arrayRetorno
       })
+    })
+    .then((objeto)=>{
+      connectToPostgres(process.env.PG_URI)
+        .then(client => insertResult(client,event,objecto)
+        .then(result=>callback(null)
     })
     .catch(err => {
       console.log(`Erro ao conectar no realizar query. Erro:`);
@@ -51,11 +68,24 @@ const processEvent = (event, context, callback) => {
     })
 }
 
+const insertResult = (client,event,objeto)=>{
+  let { protocol } = JSON.parse(event.body)
+  let sql = 
+  `
+    UPDATE notificacoes_historico 
+      SET situacao = PROCESSADO,dados = $1, 
+  `
+  let params = [objeto.dados]
+  client.query(sql,params,(err,result)=>{
+    console.log(result);
+  })
+
+}
 const queryDatabase = (db, event) => {
   let { protocol } = JSON.parse(event.body)
   console.log(protocol);
   return db.collection(process.env.MONGODB_COLLECTION).findOne({ protocol })
-    .then(result => { return result })
+    .then(result => { console.log(result);return result })
     .catch(err => { return err })
 }
 
